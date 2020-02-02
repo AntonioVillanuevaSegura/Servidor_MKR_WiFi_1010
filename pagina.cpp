d1294c1719faae69bc22db0c10890a5162753a09
Antonio Villanueva Segura <karpvs@yahoo.fr>
2020-02-02 18:33:40 +0100

    modificacion tamano botones y texto de la pagina

--- pagina.cpp
+++ pagina.cpp
@@ -13,18 +13,22 @@ void paginaWeb(WiFiClient client){
   client.println("<head>");
   client.println("</head>");
   client.println("<body>");
-  client.println("<h1 align='center'>ICARO</h1><h3 align='center'>Servidor web control salidas</h3>");
+  client.println("<h1 align='center'>ICARO</h1><h2 align='center'>Servidor web control salidas</h2>");
 
   //Crea botones .parametros se envian a traves de ?
   for (uint8_t pin=0;pin<=7;pin++){//Crea los elementos para Encender/Apagar los LEDS con los numeros
     client.println("<div style='text-align:center;'>");
 
-    client.println("<button onClick=location.href=\"/"+String(pin)+"_ON"+"\" style='margin:auto;background-color: #24A807;color: snow;padding: 10px;border: 1px solid #3F7CFF;width:65px;'>");
+    client.println("<button onClick=location.href=\"/"+String(pin)+"_ON"+"\" style='margin:auto;background-color: #24A807;color: snow;padding: 10px;border: 1px solid #3F7CFF;width:150px;'>");
+    client.println("<font size=\"+4\"> ");
     client.println("ON  "+String(pin));//Texto en boton
+    client.println("</font>");    
     client.println("</button>");
     
-    client.println("<button onClick=location.href=\"/"+String(pin)+"_OFF"+"\" style='margin:auto;background-color: #BA0B25;color: snow;padding: 10px;border: 1px solid #3F7CFF;width:65px;'>");
+    client.println("<button onClick=location.href=\"/"+String(pin)+"_OFF"+"\" style='margin:auto;background-color: #BA0B25;color: snow;padding: 10px;border: 1px solid #3F7CFF;width:150px;'>");
+    client.println("<font size=\"+4\"> ");
     client.println("OFF "+String(pin));//Texto en boton
+    client.println("</font>");    
     client.println("</button>");
     
     client.println("<br /><br />");
